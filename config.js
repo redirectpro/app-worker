7212606f07d38759c63549b95ed450e5dd266ebb
Udlei Nati <udlei@protonmail.ch>
2017-04-12 17:11:28 +0200

    AWS_S3_REGION to AWS_REGION

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   'redisHost': process.env.REDIS_HOST || '127.0.0.1',
   'redisPort': process.env.REDIS_PORT || '6379',
-  'awsRegion': process.env.AWS_S3_REGION || 'us-central-1',
+  'awsRegion': process.env.AWS_REGION || 'us-central-1',
   'awsS3Bucket': process.env.AWS_S3_BUCKET || 'redirectpro-development'
 }
 
