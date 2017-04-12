6098d253d4ac2fd7604184af6974e47d47824f30
Udlei Nati <udlei@protonmail.ch>
2017-04-12 15:34:20 +0200

    Push to docker

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker build --rm=false -t redirectpro/worker:${CIRCLE_SHA1:1:10} .
-docker push redirectpro/worker:${CIRCLE_SHA1:1:10}
+docker build --rm=false -t redirectpro/worker:${CIRCLE_TAG:1:10} .
+docker push redirectpro/worker:${CIRCLE_TAG:1:10}
