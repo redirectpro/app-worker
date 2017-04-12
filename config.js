a598ab2861da116ddb42a7f6b030e7621a9cfc91
Udlei Nati <udlei@protonmail.ch>
2017-04-12 16:38:45 +0200

    Set was

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const config = {
   'redisHost': process.env.REDIS_HOST || '127.0.0.1',
   'redisPort': process.env.REDIS_PORT || '6379',
-  'awsRegion': 'us-east-1'
+  'awsRegion': process.env.AWS_S3_REGION || 'us-central-1',
+  'awsS3Bucket': process.env.AWS_S3_BUCKET || 'redirectpro-development'
 }
 
 export default config
