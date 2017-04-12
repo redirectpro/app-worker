614847f87042fa091ce9be1424dde354a725c89c
Udlei Nati <udlei@protonmail.ch>
2017-04-12 20:57:51 +0200

    Finish deploy on ecs

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,22 @@
-docker build --rm=false -t redirectpro/worker:${CIRCLE_TAG:1:10} .
-docker push redirectpro/worker:${CIRCLE_TAG:1:10}
+export TAG=${CIRCLE_TAG:1:10}
+export IMAGE=$DOCKER_REPOSITORY:$TAG
+
+# PREPARE AND SEND TO EC2CONTAINER REPOSITORY
+eval $(aws ecr get-login --region $AWS_REGION)
+docker build --rm=false -t $IMAGE .
+docker tag $IMAGE $DOCKER_HOST/$IMAGE
+docker push $DOCKER_HOST/$IMAGE
+
+# UPDATE TASK REVISION
+aws ecs list-task-definitions --family-prefix $ECS_TASK
+export LIST=$(aws ecs list-task-definitions --family-prefix $ECS_TASK)
+export DEFINITION=$(node -e "console.log($LIST.taskDefinitionArns.reverse()[0])")
+export DESCRIBE=$(aws ecs describe-task-definition --task-definition $DEFINITION)
+export UPD_CONTAINER=$(node -e "var c=$DESCRIBE.taskDefinition.containerDefinitions; c[0].image = '$IMAGE'; console.log(JSON.stringify(c))")
+export NEW_DESCRIBE=$(aws ecs register-task-definition --family $ECS_TASK --container-definitions $UPD_CONTAINER)
+#export NEW_REVISION=$(node -e "console.log($NEW_DESCRIBE.taskDefinition.revision)")
+export NEW_LIST=$(aws ecs list-task-definitions --family-prefix $ECS_TASK)
+export NEW_DEFINITION=$(node -e "console.log($LIST.taskDefinitionArns.reverse()[0])")
+
+# UPDATE SERVICE
+aws ecs update-service --cluster $ECS_CLUSTER --service $ECS_SERVICE --task-definition $NEW_DEFINITION
