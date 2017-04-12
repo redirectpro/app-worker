8f2ae1070deac80be3c0f7d05046b10e355f4089
Udlei Nati <udlei@protonmail.ch>
2017-04-12 17:22:07 +0200

    Fixing to production

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const config = {
   'redisHost': process.env.REDIS_HOST || '127.0.0.1',
   'redisPort': process.env.REDIS_PORT || '6379',
   'awsRegion': process.env.AWS_REGION || 'eu-central-1',
-  'awsS3Bucket': 'redirectpro-development' || process.env.AWS_S3_BUCKET,
-  'dynamodbPrefix': 'rp_dev_' || process.env.DYNAMODB_PREFIX,
+  'awsS3Bucket': process.env.AWS_S3_BUCKET || 'redirectpro-development',
+  'dynamodbPrefix': process.env.DYNAMODB_PREFIX || 'rp_dev_',
   'loggerLevel': process.env.LOGGER_LEVEL || 'verbose'
 }
 
